2dd96e3c30aa2ad39736cce7acca4e5d63a120ef
lemos000 <lemosgabriel2016@gmail.com>
2025-06-04 21:42:59 -0300

    cors configured

--- src/main/java/br/com/fiap/gs/eliza/auth/CorsConfig.java
+++ src/main/java/br/com/fiap/gs/eliza/auth/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:8081", "http://localhost:19006", "exp://*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
