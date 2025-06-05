ac9d4df285fc5ce7423ba496d36911c55817bdce
lemos000 <lemosgabriel2016@gmail.com>
2025-06-04 22:07:22 -0300

    cors fixed

--- src/main/java/br/com/fiap/gs/eliza/auth/CorsConfig.java
+++ src/main/java/br/com/fiap/gs/eliza/auth/CorsConfig.java
@@ -7,14 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081", "http://localhost:19006", "exp://*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
